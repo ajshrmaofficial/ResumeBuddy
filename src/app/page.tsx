fc4c42eef77d81f71aa46a32267992dc2d6da31b
Ajay Sharma <ajshrmaofficial@gmail.com>
2025-02-17 00:44:05 +0530

    initial commit
    - tailwind setup & initial test changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import "./globals.css";
 
 export default function Home() {
   return (
@@ -13,7 +14,7 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol>
+        <ol className="text-green-500">
           <li>
             Get started by editing <code>src/app/page.tsx</code>.
           </li>
